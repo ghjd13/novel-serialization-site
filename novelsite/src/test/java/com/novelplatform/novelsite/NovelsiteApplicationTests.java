32ce37b0c42794cf4e57a21d2266544fd876a2a3
ghjd13 <69751035+ghjd13@users.noreply.github.com>
2025-10-12 10:37:53 +0900

    Spring 생성

---
+++ novelsite/src/test/java/com/novelplatform/novelsite/NovelsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novelplatform.novelsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
